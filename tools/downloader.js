58c7ef5a63f133b990c54d4e274088f1b87c7edc
mugglecloud <mugglecloud@163.com>
2020-04-07 12:04:42 +0800

    .

--- tools/downloader.js
+++ tools/downloader.js
@@ -8,37 +8,34 @@ function getUrl(num) {
   return `http://feedmusic.com/images/frame-high/${num}.jpg`;
 }
 
-request("http://www.google.com", function (error, response, body) {});
-
-function writeFile(url, outDir, outPath) {
-  request(url, (error, response, body) => {
-    if (error) {
-      console.error(error.message);
-      return;
-    }
-
-    console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-
-    fs.mkdir(outDir, { recursive: true }, (err) => {
-      if (err) console.error(err);
-
-      fs.writeFileSync(outPath, body);
-    });
+function writeFile(uri, filename) {
+  return new Promise((resolve, reject) => {
+    request(uri)
+      .on("error", reject)
+      .pipe(fs.createWriteStream(filename))
+      .on("finish", () => {
+        console.log("write file", filename, "success");
+        resolve();
+      });
   });
 }
 
 function download() {
+  let promises = [];
   for (let i = 1; i < 180; i++) {
     const url = getUrl(i);
     const outPath = path.join(OssPath, url.replace("http://", ""));
-    const outDir = path.dirname(outPath);
 
     fs.access(outPath, fs.constants.R_OK | fs.constants.W_OK, (accessErr) => {
       if (!accessErr) return;
 
-      writeFile(url, outDir, outPath);
+      promises.push(writeFile(url, outPath));
     });
+
+    // promises.push(writeFile(url, outPath));
   }
+
+  Promise.all(promises);
 }
 
 download();
