72e20376d423b73204898ae1a01e71eff11d2bb4
mugglecloud <mugglecloud@163.com>
2020-04-14 10:37:53 +0800

    .

---
+++ precache-manifest.0e37055aede35b015f1b74d67cc59c88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e02ba52d92af9885f1a657d30f19eb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c197c573d712bb53db59",
+    "url": "./static/css/main.3a487631.chunk.css"
+  },
+  {
+    "revision": "908857535cef655df11c",
+    "url": "./static/js/2.ea50a519.chunk.js"
+  },
+  {
+    "revision": "729937898630e839994162d5d9f8ce89",
+    "url": "./static/js/2.ea50a519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c197c573d712bb53db59",
+    "url": "./static/js/main.8294bb2d.chunk.js"
+  },
+  {
+    "revision": "e47a329598c3b0f6cfce",
+    "url": "./static/js/runtime-main.9b0830dc.js"
+  }
+]);
